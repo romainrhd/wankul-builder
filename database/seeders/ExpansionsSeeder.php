fddaf43df3d75a4a998f431a33f0e4447f4594d0
Romain <romain.richard.19@gmail.com>
2024-01-07 23:53:53 +0100

    feat(expansion-card): add expansion resource

---
+++ database/seeders/ExpansionsSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Expansion;
+use Illuminate\Database\Seeder;
+
+class ExpansionsSeeder extends Seeder
+{
+    private const EXPANSIONS = [
+        'Origins', 'Campus', 'Hors Série',
+    ];
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach (self::EXPANSIONS as $expansion) {
+            Expansion::create([
+                'name' => $expansion,
+            ]);
+        }
+    }
+}
